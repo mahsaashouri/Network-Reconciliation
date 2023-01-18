ff283390c4cb2dc1f15dd75b9deca530bbdb23c3
Mahsa Ashouri <mahsa.ashouri@iss.nthu.edu.tw>
2023-01-18 14:57:13 -0500

    Updated cleaning data file - randomly sample from rows

--- Data-cleaning-sampling.R
+++ Data-cleaning-sampling.R
@@ -1,11 +1,20 @@
 
 library(tidyverse)
-## reding data
+## reading data
 library(data.table)
 click.2022.12<-as.data.frame(fread("clickstream-enwiki-2022-12.tsv"))
 ## specify column names
 colnames(click.2022.12) <- c('prev', 'curr', 'type', 'freq')
 
+## make all the outflows from one source
+click.2022.12  <- data.frame(lapply(click.2022.12, sub, pattern = "other-.*", replacement = "other"))
+
 ## add id column
-click.2022.12 %>%
-  mutate('id' = )
+click.2022.12 <- click.2022.12 %>%
+  mutate('id' = paste(prev, curr, sep = ':'))
+
+## sample 3000 rows
+set.seed(123)
+click.2022.12.sample <- click.2022.12 %>% slice_sample(n = 300)
+
+
