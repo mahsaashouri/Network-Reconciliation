f7233b0f75740f46a09fe52646e4fded46a68631
Mahsa Ashouri <mahsa.ashouri@iss.nthu.edu.tw>
2023-02-14 16:30:17 -0500

    Updated the smatrix by writing that as a function

--- Smatrix-SmallExample.R
+++ Smatrix-SmallExample.R
@@ -23,33 +23,76 @@ number.row <- 1+1+ifelse(length((filter(data.all, sub("\\..$", "", data.all$cat)
 
 smatrix <- matrix(NA, ncol = length(unique(data.all$cat)), nrow = number.row)
 
-# total IN 
-smatrix[1,] <- rep(1, ncol(smatrix))
-# total OUT
-smatrix[2,] <- c(rep(1, ncol(smatrix)-length(unique(filter(data.all, sub("\\..$", "", data.all$cat)=='O')$cat))), 
-                 rep(0, length(unique(filter(data.all, sub("\\..$", "", data.all$cat)=='O')$cat))))
-# total Outer series (other)
-smatrix[3,] <- c(rep(0, ncol(smatrix)-length(unique(filter(data.all, sub("\\..$", "", data.all$cat)=='O')$cat))), 
-                 rep(1, length(unique(filter(data.all, sub("\\..$", "", data.all$cat)=='O')$cat))))
-# IN series
-no.in.series <- ifelse(sum(unique(sub("\\..$", "", data.all$cat)) %in% "O"),length(unique(sub("\\..$", "", data.all$cat)))-1, 
-                       length(unique(sub("\\..$", "", data.all$cat))))
-for(i in 1:no.in.series){
-  s.in <- unique(data.all$cat)
-  s.in.q <- unique(sub("*..", "", s.in))
-  smatrix[3+i,] <- ifelse(sub("*..", "", s.in) %in% s.in.q[i], 1, 0)
+
+
+## Generating sample series
+
+A.B <- round(runif(10),2)
+A.C <- round(runif(10),2)
+B.A <- round(runif(10),2)
+B.C <- round(runif(10),2)
+C.A <- round(runif(10),2)
+C.B <- round(runif(10),2)
+O.A <- round(runif(10),2)
+O.B <- round(runif(10),2)
+O.C <- round(runif(10),2)
+
+data.network <- dplyr::bind_rows(list(A.B = A.B, A.C = A.C, B.A = B.A, B.C = B.C, C.A = C.A, C.B = C.B, 
+                                      O.A = O.A, O.B = O.B, O.C = O.C), .id = 'id')
+data.network <- reshape2::melt(data.network)
+colnames(data.network) <- c('cat', 'series')
+
+## input should be a dataframe with two columns 1- series 2- cat which shows the name of the series 
+## structure of the names: 'first series.second series' - 'O' shows the 'Outer' series 
+
+smatrix <- function(data.network){
+  
+  ## series before and after '.' which shows inner and outer series
+  char.before <- sub("\\..$", "", data.network$cat)
+  char.after <- sub("*..", "", data.network$cat)
+  
+  ## number of rows in smatrix.network
+  number.row <- 1 + 1 + ifelse(length((filter(data.network, char.before =='O'))$cat)!=0, 1, 0) +
+    ifelse(sum(unique(char.before) %in% "O"),length(unique(char.before))-1, length(unique(char.before))) + 
+    length(unique(char.after)) + length(unique(data.network$cat))
+  
+  ## emty matrix for smatrix
+  smatrix.network <- matrix(NA, ncol = length(unique(data.network$cat)), nrow = number.row)
+  # total IN 
+  smatrix.network[1,] <- rep(1, ncol(smatrix.network))
+  # total OUT
+  smatrix.network[2,] <- c(rep(1, ncol(smatrix.network)-length(unique(filter(data.network, char.before == 'O')$cat))), 
+                           rep(0, length(unique(filter(data.network, char.before == 'O')$cat))))
+  # total Outer series (other)
+  if(sum(unique(char.before) %in% "O") == 1){
+    h <- 3
+    smatrix.network[h,] <- c(rep(0, ncol(smatrix.network)-length(unique(filter(data.network, char.before == 'O')$cat))), 
+                             rep(1, length(unique(filter(data.network, char.before == 'O')$cat))))  
+  }
+  else{
+    h <- 0
+  }
+  
+  cat.un <-  unique(data.network$cat)
+  # IN series
+  no.in.series <- ifelse(sum(unique(char.before) %in% "O"),length(unique(char.before))-1, 
+                         length(unique(char.before)))
+  for(i in 1:no.in.series){
+    s.in <- unique(char.after)
+    smatrix.network[h+i,] <- ifelse(sub("*..", "", cat.un) %in% s.in[i], 1, 0)
+  }
+  
+  # OUT series
+  no.out.series <- length(unique(char.after))
+  for(i in 1:no.out.series){
+    s.out <- unique(char.before)[!unique(char.before) %in% "O"]
+    smatrix.network[h+no.in.series+i,] <- ifelse(sub("\\..$", "", cat.un) %in% s.out[i], 1, 0)
+  }
+  # Bottom level series
+  smatrix.network[(h+no.in.series+no.out.series+1):number.row,] <- diag(1, length(unique(data.network$cat)))
+  
+  return(smatrix.network)
 }
 
-# OUT series
-no.out.series <- length(unique(sub("*..", "", data.all$cat)))
-for(i in 1:no.out.series){
-  s.out <- unique(data.all$cat)
-  s.out.q.1 <- unique(sub("\\..$", "", data.all$cat))
-  s.out.q <- s.out.q.1[!s.out.q.1 %in% "O"]
-  smatrix[3+no.in.series+i,] <- ifelse(sub("\\..$", "", s.out) %in% s.out.q[i], 1, 0)
-}
-
-
-# Bottom level series
-
-smatrix[(3+no.in.series+no.out.series+1):number.row,] <- diag(1, length(unique(data.all$cat)))
+## run the function
+smatrix(data.network = data.all)
