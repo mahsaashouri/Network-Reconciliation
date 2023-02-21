eb6d3a7d38ab76e4dfe0ced8c8c03e944c21cee4
Mahsa Ashouri <mahsa.ashouri@iss.nthu.edu.tw>
2023-02-21 16:05:30 -0500

    Addedmint shrink reconciliation method

--- SimulateSeriesCorr.R
+++ SimulateSeriesCorr.R
@@ -73,24 +73,56 @@ ngts.net <- ts(Aggreg.func(data.network), frequency = 12, start = c(2010, 1))
 net.train <- window(ngts.net, end = c(2018, 12))
 net.test <- window(ngts.net, start = c(2019, 1))
 h <- 12 ## number of forecast points
-fc <- matrix(NA, nrow = nrow(net.test), ncol = ncol(net.test))
+fc.arima <- matrix(NA, nrow = nrow(net.test), ncol = ncol(net.test))
+train.error <- matrix(NA, nrow = nrow(net.train), ncol = ncol(net.train))
 for(i in seq(NCOL(net.train)))
 {
-  fc[,i] <- forecast(auto.arima(net.train[,i]), h = h)$mean
+  fc <- forecast(auto.arima(net.train[,i]), h = h)
+  fc.arima[,i] <- fc$mean
+  train.error[,i] <- fc$fitted - net.train[,i]
 }
-colnames(fc) <- colnames(net.test)
+colnames(fc.arima) <- colnames(net.test)
+colnames(train.error) <- colnames(net.train)
 
-## computing reconciliation matrix
+## computing reconciliation matrix - simplest type
 lambda <- diag(rowSums(smatrix.net))
 
 rec.adj.lambda <- as.matrix(smatrix.net%*%solve(t(smatrix.net)%*%solve(lambda)%*%smatrix.net)%*%t(smatrix.net)%*%solve(lambda))
 
-fc.rec <- matrix(NA, nrow = 12, ncol = ncol(net.test))
-for(i in 1:nrow(fc)){
-  f.1 <- matrix(as.numeric(fc[i,]), ncol = 1, nrow = ncol(fc))
-  fc.rec [i,] <- rec.adj.lambda %*% f.1
+fc.arima.rec <- matrix(NA, nrow = 12, ncol = ncol(net.test))
+for(i in 1:nrow(fc.arima)){
+  f.1 <- matrix(as.numeric(fc.arima[i,]), ncol = 1, nrow = ncol(fc.arima))
+  fc.arima.rec [i,] <- rec.adj.lambda %*% f.1
 }
-colnames(fc.rec ) <- colnames(net.test)
+colnames(fc.arima.rec ) <- colnames(net.test)
+
+## computing reconciliation matrix - mint_shrink 
+n <- nrow(train.error)
+covm <- crossprod(stats::na.omit(train.error)) / n
+tar <- diag(apply(train.error, 2, compose(crossprod, stats::na.omit))/n)
+corm <- cov2cor(covm)
+xs <- scale(train.error, center = FALSE, scale = sqrt(diag(covm)))
+xs <- xs[stats::complete.cases(xs),]
+v <- (1/(n * (n - 1))) * (crossprod(xs^2) - 1/n * (crossprod(xs))^2)
+diag(v) <- 0
+corapn <- cov2cor(tar)
+d <- (corm - corapn)^2
+lambda <- sum(v)/sum(d)
+lambda <- max(min(lambda, 1), 0)
+W <- lambda * tar + (1 - lambda) * covm
+R <- t(smatrix.net)%*%solve(W)
+P <- Matrix::solve(R%*%smatrix.net)%*%R
+SP <- as.matrix(smatrix.net%*%P)
+
+## multiply the based forecasts by SP matrix
+
+fc.mint.shrink.arima <- matrix(NA, nrow = h, ncol = ncol(net.test))
+
+for(i in 1:nrow(fc.arima)){
+  f.1 <- matrix(as.numeric(fc.arima[i,]), ncol = 1, nrow = ncol(fc.arima))
+  fc.mint.shrink.arima [i,] <- SP %*% f.1
+}
+colnames(fc.mint.shrink.arima) <- colnames(net.test)
 
 
 
