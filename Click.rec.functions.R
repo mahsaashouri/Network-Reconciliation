1ed9c0c67040d2110ff8872835bfbc9434e1c102
Mahsa Ashouri <mahsa.ashouri@iss.nthu.edu.tw>
2023-03-01 17:33:33 -0500

    Added part to plot results in ggplot

--- Click.rec.functions.R
+++ Click.rec.functions.R
@@ -150,9 +150,9 @@ ngts.net <- ts(as.matrix(Aggreg.func(data.network)), frequency = 12, start = c(2
 
 ## training and test sets
 # Splitting data into training and test sets
-net.train <- window(ngts.net, end = c(2022, 6))
-net.test <- window(ngts.net, start = c(2022, 7))
-h <- 6 ## number of forecast points
+net.train <- window(ngts.net, end = c(2021, 12))
+net.test <- window(ngts.net, start = c(2022, 1))
+h <- 12 ## number of forecast points
 fc.arima <- matrix(NA, nrow = nrow(net.test), ncol = ncol(net.test))
 train.error <- matrix(NA, nrow = nrow(net.train), ncol = ncol(net.train))
 for(i in seq(NCOL(net.train))){
@@ -204,12 +204,90 @@ for(i in 1:nrow(fc.arima)){
 colnames(fc.mint.shrink.arima) <- colnames(net.test)
 
 
-## plot some results
-plot(c(net.test[,'Battle_of_the_Bismarck_Sea.in']), type = 'l')
-lines(c(fc.arima[,'Battle_of_the_Bismarck_Sea.in']), col = 'green')
-lines(c(fc.arima.rec[,'Battle_of_the_Bismarck_Sea.in']), col = 'blue')
-lines(c(fc.mint.shrink.arima[,'Battle_of_the_Bismarck_Sea.in']), col = 'red')
+# bind the datasARIMA into a single data frame
+merged_arima <- rbind(cbind(reshape2::melt(net.test), data = 'test.set'), cbind(reshape2::melt(fc.arima), data = 'arima'),
+                    cbind(reshape2::melt(fc.arima.rec), data = 'arima.rec'), 
+                    cbind(reshape2::melt(fc.mint.shrink.arima), data = 'mint.shrink.arima'))
 
+merged_arima%>%
+  filter(Var2 == "Total.in") %>%
+  ggplot(aes(x = Var1, y = value, colour = data)) +
+  geom_line() +
+  xlab("Horizon") +
+  ylab("Count") +
+  theme_bw()
+
+
+## base forecasts using ETS
+
+## training and test sets
+# Splitting data into training and test sets
+net.train <- window(ngts.net, end = c(2022, 6))
+net.test <- window(ngts.net, start = c(2022, 7))
+h <- 6 ## number of forecast points
+fc.ETS <- matrix(NA, nrow = nrow(net.test), ncol = ncol(net.test))
+train.error <- matrix(NA, nrow = nrow(net.train), ncol = ncol(net.train))
+for(i in seq(NCOL(net.train))){
+  fc <- forecast(ets(net.train[,i]), h = h)
+  fc.ETS[,i] <- fc$mean
+  train.error[,i] <- fc$fitted - net.train[,i]
+}
+colnames(fc.ETS) <- colnames(net.test)
+colnames(train.error) <- colnames(net.train)
+
+## computing reconciliation matrix - simplest type
+lambda <- diag(rowSums(smatrix.net))
+
+rec.adj.lambda <- as.matrix(smatrix.net%*%solve(t(smatrix.net)%*%solve(lambda)%*%smatrix.net)%*%t(smatrix.net)%*%solve(lambda))
+
+fc.ETS.rec <- matrix(NA, nrow = h, ncol = ncol(net.test))
+for(i in 1:nrow(fc.ETS)){
+  f.1 <- matrix(as.numeric(fc.ETS[i,]), ncol = 1, nrow = ncol(fc.ETS))
+  fc.ETS.rec [i,] <- rec.adj.lambda %*% f.1
+}
+colnames(fc.ETS.rec ) <- colnames(net.test)
+
+## computing reconciliation matrix - mint_shrink 
+n <- nrow(train.error)
+covm <- crossprod(stats::na.omit(train.error)) / n
+tar <- diag(apply(train.error, 2, compose(crossprod, stats::na.omit))/n)
+corm <- cov2cor(covm)
+xs <- scale(train.error, center = FALSE, scale = sqrt(diag(covm)))
+xs <- xs[stats::complete.cases(xs),]
+v <- (1/(n * (n - 1))) * (crossprod(xs^2) - 1/n * (crossprod(xs))^2)
+diag(v) <- 0
+corapn <- cov2cor(tar)
+d <- (corm - corapn)^2
+lambda <- sum(v)/sum(d)
+lambda <- max(min(lambda, 1), 0)
+W <- lambda * tar + (1 - lambda) * covm
+R <- t(smatrix.net)%*%solve(W)
+P <- Matrix::solve(R%*%smatrix.net)%*%R
+SP <- as.matrix(smatrix.net%*%P)
+
+## multiply the based forecasts by SP matrix
+
+fc.mint.shrink.ETS <- matrix(NA, nrow = h, ncol = ncol(net.test))
+
+for(i in 1:nrow(fc.ETS)){
+  f.1 <- matrix(as.numeric(fc.ETS[i,]), ncol = 1, nrow = ncol(fc.ETS))
+  fc.mint.shrink.ETS [i,] <- SP %*% f.1
+}
+colnames(fc.mint.shrink.ETS) <- colnames(net.test)
+
+
+# bind the datasets into a single data frame
+merged_ETS <- rbind(cbind(reshape2::melt(net.test), data = 'test.set'), cbind(reshape2::melt(fc.ETS), data = 'ETS'),
+                    cbind(reshape2::melt(fc.ETS.rec), data = 'ETS.rec'), 
+                    cbind(reshape2::melt(fc.mint.shrink.ETS), data = 'mint.shrink.ETS'))
+
+merged_ETS%>%
+ filter(Var2 == "Innale.out") %>%
+  ggplot(aes(x = Var1, y = value, colour = data)) +
+  geom_line() +
+  xlab("Horizon") +
+  ylab("Count") +
+  theme_bw()
 
 
 
