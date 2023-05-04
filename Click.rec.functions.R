f786218cc1070d26b1097534ae25412dc9356d5f
Mahsa Ashouri <mahsa.ashouri@iss.nthu.edu.tw>
2023-05-04 17:23:26 -0400

    Changed separator between curr and prev titles

--- Click.rec.functions.R
+++ Click.rec.functions.R
@@ -23,8 +23,8 @@ library(purrr)
 smatrix <- function(data.network){
   
   ## series before and after '.' which shows inner and outer series
-  char.before <- sub(":.*", "", data.network$cat)
-  char.after <- sub(".*:", "", data.network$cat)
+  char.before <- sub("::.*", "", data.network$cat)
+  char.after <- sub(".*::", "", data.network$cat)
   
   ## number of rows in smatrix.network
   number.row <- 1 + 1 + ifelse(length((filter(data.network, char.before =='other'))$cat)!=0, 1, 0) +
@@ -53,7 +53,7 @@ smatrix <- function(data.network){
   no.in.series <- length(unique(char.after))
   for(i in 1:no.in.series){
     s.in <- unique(char.after)
-    smatrix.network[h+i,] <- ifelse(sub(".*:", "", cat.un) %in% s.in[i], 1, 0)
+    smatrix.network[h+i,] <- ifelse(sub(".*::", "", cat.un) %in% s.in[i], 1, 0)
   }
   
   # OUT series
@@ -61,7 +61,7 @@ smatrix <- function(data.network){
                          length(unique(char.before)))
   for(i in 1:no.out.series){
     s.out <- unique(char.before)[!unique(char.before) %in% "other"]
-    smatrix.network[h+no.in.series+i,] <- ifelse(sub(":.*", "", cat.un) %in% s.out[i], 1, 0)
+    smatrix.network[h+no.in.series+i,] <- ifelse(sub("::.*", "", cat.un) %in% s.out[i], 1, 0)
   }
   # Bottom level series
   smatrix.network[(h+no.in.series+no.out.series+1):number.row,] <- diag(1, length(unique(data.network$cat)))
@@ -83,7 +83,7 @@ Aggreg.func <- function(data.network){
   
   # total OUT 
   TotalOut <- data.network %>%
-    filter(sub(":.*", "", cat)!='other') %>%
+    filter(sub("::.*", "", cat)!='other') %>%
     group_split(cat) %>%
     map(~.[['series']]) %>%
     reduce(`+`)
@@ -92,7 +92,7 @@ Aggreg.func <- function(data.network){
   
   # total Outer series (other)
   Outer <- data.network %>%
-    filter(sub(":.*", "", cat)=='other') %>%
+    filter(sub("::.*", "", cat)=='other') %>%
     group_split(cat) %>%
     map(~.[['series']]) %>%
     reduce(`+`)
@@ -101,7 +101,7 @@ Aggreg.func <- function(data.network){
   
   # IN series
   DataIn <- data.network %>%
-    mutate('curr.id'= factor(sub(".*:", "", cat), level = unique(sub(".*:", "", cat)))) %>%
+    mutate('curr.id'= factor(sub(".*::", "", cat), level = unique(sub(".*::", "", cat)))) %>%
     group_split(curr.id) 
   
   SumIn <- Matrix(0, nrow = length(TotalIn), ncol = length(DataIn), sparse = TRUE); NameIn <- c()
@@ -121,8 +121,8 @@ Aggreg.func <- function(data.network){
   
   # OUT series
   DataOut <- data.network %>%
-    filter(sub(":.*", "", cat)!='other') %>%
-    mutate( 'prev.id'= factor(sub(":.*", "", cat), level = unique(sub(":.*", "", cat)))) %>%
+    filter(sub("::.*", "", cat)!='other') %>%
+    mutate( 'prev.id'= factor(sub("::.*", "", cat), level = unique(sub("::.*", "", cat)))) %>%
     group_split(prev.id) 
   
   SumOut <- Matrix(0, nrow = length(TotalOut), ncol = length(DataOut), sparse = TRUE); NameOut <- c()
