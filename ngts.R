d2e8a3233e9d0057ff986fe6bd32a5e77f6fbb66
Mahsa Ashouri <mahsa.ashouri@iss.nthu.edu.tw>
2023-06-06 21:02:59 -0400

    Made ngts and smatrix as two functions and read it in the main file

---
+++ ngts.R
@@ -0,0 +1,79 @@
+## aggregation function
+Aggreg.func <- function(data.network){
+  char.before <- sub("::.*", "", data.network$cat)
+  char.after <- sub(".*::", "", data.network$cat)
+  # total IN 
+  TotalIn <- data.network %>%
+    group_split(cat) %>%
+    map(~.[['series']]) %>%
+    reduce(`+`)
+  TotalIn <- Matrix(TotalIn, nrow = length(TotalIn), sparse = TRUE)
+  colnames(TotalIn) <- 'Total.in'
+  
+  # total OUT 
+  TotalOut <- data.network %>%
+    filter(char.before!='other') %>%
+    group_split(cat) %>%
+    map(~.[['series']]) %>%
+    reduce(`+`)
+  TotalOut <- Matrix(TotalOut, nrow = length(TotalOut), sparse = TRUE)
+  colnames(TotalOut) <- 'Total.out'
+  
+  # total Outer series (other)
+  Outer <- data.network %>%
+    filter(char.before =='other') %>%
+    group_split(cat) %>%
+    map(~.[['series']]) %>%
+    reduce(`+`)
+  Outer <- Matrix(Outer, nrow = length(Outer), sparse = TRUE)
+  colnames(Outer) <- 'Outer'
+  
+  # IN series
+  DataIn <- data.network %>%
+    mutate('curr.id'= factor(char.after, level = unique(char.after))) %>%
+    group_split(curr.id) 
+  
+  SumIn <- Matrix(0, nrow = length(TotalIn), ncol = length(DataIn), sparse = TRUE); NameIn <- c()
+  
+  for(i in 1:length(DataIn)){
+    name.in <- unique(as.character(DataIn[[i]]$curr.id))
+    NameIn <- c(NameIn, name.in)
+  }
+  colnames(SumIn) <- paste(NameIn,'in',sep='.')
+  
+  for (i in 1:length(DataIn)){
+    SumIn[,i] <- DataIn[[i]]%>%
+      group_split(cat) %>%
+      map(~.[['series']]) %>%
+      reduce(`+`)
+  }
+  # OUT series
+  DataOut <- data.network %>%
+    filter(char.before!='other') %>%
+    mutate( 'prev.id'= factor(sub("::.*", "", cat), level = unique(sub("::.*", "", cat)))) %>%
+    group_split(prev.id) 
+  
+  SumOut <- Matrix(0, nrow = length(TotalOut), ncol = length(DataOut), sparse = TRUE); NameOut <- c()
+  
+  for(i in 1:length(DataOut)){
+    name.out <- unique(as.character(DataOut[[i]]$prev.id))
+    NameOut <- c(NameOut, name.out)
+  }
+  colnames(SumOut) <- paste(NameOut,'out',sep='.')
+  
+  for (i in 1:length(DataOut)){
+    SumOut[,i] <- DataOut[[i]]%>%
+      group_split(cat) %>%
+      map(~.[['series']]) %>%
+      reduce(`+`)
+  }
+  # Bottom level series
+  BottomLevel <- Matrix(as.numeric(data.network$series), nrow = nrow(TotalIn), sparse = TRUE)
+  colnames(BottomLevel) <- unique(data.network$cat)
+  
+  ## Final aggregated matrix
+  
+  AggregMat <- cbind(TotalIn, TotalOut, Outer, SumIn, SumOut, BottomLevel)
+  
+  return(AggregMat)
+}
---
+++ smatrix.R
@@ -0,0 +1,62 @@
+## Summing matrix
+smatrix <- function(data.network) {
+  # Preprocess data to avoid redundant computations
+  char.before <- sub("::.*", "", data.network$cat)
+  char.after <- sub(".*::", "", data.network$cat)
+  unique_cat <- unique(data.network$cat)
+  other_cat <- unique(filter(data.network, char.before == 'other')$cat)
+  
+  # Calculate required dimensions and preallocate memory
+  number.row <- 1 + 1 + ifelse(length((filter(data.network, char.before == 'other'))$cat) != 0, 1, 0) +
+    ifelse(sum(unique(char.before) %in% "other"), length(unique(char.before)) - 1, length(unique(char.before))) +
+    length(unique(char.after)) + length(unique(data.network$cat))
+  #smatrix.network <- Matrix(0, ncol = length(unique_cat), nrow = number.row, sparse = TRUE)
+  smatrix.network <- sparseMatrix(
+    i = numeric(0),
+    j = numeric(0),
+    x = numeric(0),
+    dims = c(number.row, length(unique(data.network$cat)))
+  )
+  
+  # total IN
+  smatrix.network[1, ] <- 1
+  
+  # total OUT
+  smatrix.network[2, ] <- c(rep(1, ncol(smatrix.network) - length(other_cat)),
+                            rep(0, length(other_cat)))
+  
+  # total Outer series (other)
+  if (sum(unique(char.before) %in% "other") == 1) {
+    h <- 3
+    smatrix.network[h, ] <- c(rep(0, ncol(smatrix.network) - length(other_cat)),
+                              rep(1, length(other_cat)))
+  } else {
+    h <- 0
+  }
+  
+  # IN series
+  no.in.series <- length(unique(char.after))
+  s.in <- unique(char.after)
+  unique_cat_after <-sub(".*::", "", unique_cat)
+  for (i in 1:no.in.series) {
+    #smatrix.network[h + i, ] <- ifelse(unique_cat_after %in% s.in[i], 1, 0)
+    smatrix.network[h + i, ] <- as.integer(unique_cat_after %in% s.in[i])
+  }
+  
+  # OUT series
+  no.out.series <- ifelse(sum(unique(char.before) %in% "other"), length(unique(char.before)) - 1,
+                          length(unique(char.before)))
+  s.out <- unique(char.before)[!unique(char.before) %in% "other"]
+  unique_cat_before <- sub("::.*", "", unique_cat)
+  for (i in 1:no.out.series) {
+    #smatrix.network[h + no.in.series + i, ] <- ifelse(unique_cat_before %in% s.out[i], 1, 0)
+    smatrix.network[h + no.in.series + i, ] <- as.integer(unique_cat_before %in% s.out[i])
+  }
+  # Bottom level series
+  #smatrix.network[(h + no.in.series + no.out.series + 1):number.row, ] <- Diagonal(1, length(unique_cat))
+  smatrix.network[(h + no.in.series + no.out.series + 1):number.row, ] <- sparseMatrix(i = 1:length(unique_cat),
+                                                                                       j = 1:length(unique_cat),
+                                                                                       x = 1,
+                                                                                       dims = c(length(unique_cat), length(unique_cat)))
+  return(smatrix.network)
+}
