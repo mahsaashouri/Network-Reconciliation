20ae4f85d4f8845410471897c3fbbcdd8c7b9c74
Mahsa Ashouri <mahsa.ashouri@iss.nthu.edu.tw>
2023-08-10 15:04:32 -0400

    Updated the running results file

--- Click.rec.functions.R
+++ Click.rec.functions.R
@@ -2,17 +2,20 @@
 library(tidyverse)
 library(forecast)
 library(Matrix)
+library(hts)
+library(SparseM)
 source('smatrix.R')
 source('ngts.R')
-source('Scale_data.R')
-source('Invert_scale.R')
+source('CG.shrink.R')
+source('olsfc.pwfc.AIC-BIC.R')
+
 
 data.network.all <- read_csv('SampleClickProduct.csv')[,-1]
 data.network <- data.network.all[,c('id', 'freq')]
 colnames(data.network) <- c('cat', 'series')
 
 ## Smooth the series
-library(purrr)
+#library(purrr)
 # split data by category and convert to time series
 #ts_list <- split(data.network, data.network$cat) %>%
 #  map(~ ts(.x$series, frequency = 12)) %>%
@@ -27,187 +30,177 @@ smatrix.net <- smatrix(data.network = data.network)
 ngts.net <- ts(as.matrix(Aggreg.func(data.network)), frequency = 12, start = c(2017, 11))
 
 ## plot the series 
-ngts.net.melt <- reshape2::melt(ngts.net)
-ngts.net.melt%>%
-  filter(Var2 == "Total.in") %>%
-  ggplot(aes(x = Var1, y = value)) +
-  geom_line() +
-  xlab("Horizon") +
-  ylab("Count") +
-  theme_bw()
+#ngts.net.melt <- reshape2::melt(ngts.net)
+#ngts.net.melt%>%
+#  filter(Var2 == "Total.in") %>%
+#  ggplot(aes(x = Var1, y = value)) +
+#  geom_line() +
+#  xlab("Horizon") +
+#  ylab("Count") +
+#  theme_bw()
 
-#plot(as.matrix(ngts.net)[,1], type = 'l')
-## base forecasts using ARIMA
 
 ## training and test sets
 # Splitting data into training and test sets
 net.train <- window(ngts.net, end = c(2022, 2))
 net.test <- window(ngts.net, start = c(2022, 3))
-h <- 12 ## number of forecast points
+write.csv(net.train, 'net.train.csv')
+write.csv(net.test, 'net.test.csv')
+h <- 12
 
-# Apply scaling to net.gts dataset
-Scaled <- scale_data(net.train, net.test, c(-1, 1))
-train.all  <- Scaled$scaled_train
-test.all <- Scaled$scaled_test
-# Access the scaled data
-y_train <- Scaled$scaled_train
-y_test <- Scaled$scaled_test
-
-
-# Apply inverse scaling to the scaled data - just to check
-#inverted_data <- invert_scaling(Scaled$scaled_train, Scaled$scaler, c(-1, 1))
-
-fc.arima <- matrix(NA, nrow = nrow(y_test), ncol = ncol(y_test))
-train.fit <- matrix(NA, nrow = nrow(y_train), ncol = ncol(y_train))
-for(i in seq(NCOL(y_train))){
-  fc <- forecast(auto.arima(train.all[,i], D = 1, d = 1), h = h)
+## ARIMA
+fc.arima <- matrix(NA, nrow = nrow(net.test), ncol = ncol(net.test))
+train.fit <- matrix(NA, nrow = nrow(net.train)-1, ncol = ncol(net.train))
+for(i in seq(NCOL(net.train))){
+  c <- forecast(auto.arima(net.train[,i], D = 1, d = 1), h = h)
   fc.arima[,i] <- fc$mean
-  train.fit[,i] <- fc$fitted #- y_train[,i]
+  train.fit[,i] <- fc$fitted 
 }
 colnames(fc.arima) <- colnames(net.test)
 colnames(train.fit) <- colnames(net.train)
-#inverted_data.test <- invert_scaling(fc.arima, Scaled$scaler, c(-1, 1))
 
-## computing reconciliation matrix - simplest type
-lambda_vector <- Matrix::rowSums(smatrix.net)
-lambda <- sparseMatrix(i = 1:length(lambda_vector),
-             j = 1:length(lambda_vector),
-             x = lambda_vector,
-             dims = c(length(lambda_vector), length(lambda_vector)))
-Inv_lambda <- solve(lambda)
-#test <- t(smatrix.net)%*%Inv_lambda
-#test2 <- test%*%smatrix.net
-#test3 <- solve(as.matrix(test2))
-Inv_smatrix.net <- solve(t(smatrix.net)%*%Inv_lambda%*%smatrix.net)
-rec.adj.lambda <- (smatrix.net%*%Inv_smatrix.net%*%t(smatrix.net)%*%Inv_lambda)
+write.csv(fc.arima, 'fc.arima.unrec.csv')
+## residuals
+res <- as.matrix(as.data.frame(net.train) - train.fit)
+write.csv(res, 'res.train.arima.csv')
+tar <- lowerD(res)
+shrink <- shrink.estim(res, tar)
+w.1 <- shrink[[1]]
+lambda <- shrink[[2]]
+weights <-  methods::as(w.1, "sparseMatrix")
 
-fc.arima.rec <- matrix(NA, nrow = h, ncol = ncol(net.test))
-for(i in 1:nrow(fc.arima)){
-  f.1 <- matrix(as.numeric(fc.arima[i,]), ncol = 1, nrow = ncol(fc.arima))
-  fc.arima.rec [i,] <- rec.adj.lambda %*% f.1
-}
-colnames(fc.arima.rec ) <- colnames(net.test)
-
-## computing reconciliation matrix - mint_shrink 
-n <- nrow(train.error)
-covm <- crossprod(stats::na.omit(train.error)) / n
-tar <- diag(apply(train.error, 2, compose(crossprod, stats::na.omit))/n)
-corm <- cov2cor(covm)
-xs <- scale(train.error, center = FALSE, scale = sqrt(diag(covm)))
-xs <- xs[stats::complete.cases(xs),]
-v <- (1/(n * (n - 1))) * (crossprod(xs^2) - 1/n * (crossprod(xs))^2)
-diag(v) <- 0
-corapn <- cov2cor(tar)
-d <- (corm - corapn)^2
-lambda <- sum(v)/sum(d)
-lambda <- max(min(lambda, 1), 0)
-W <- lambda * tar + (1 - lambda) * covm
-R <- t(smatrix.net)%*%solve(W)
-P <- Matrix::solve(R%*%smatrix.net)%*%R
-SP <- as.matrix(smatrix.net%*%P)
-
-## multiply the based forecasts by SP matrix
-
-fc.mint.shrink.arima <- matrix(NA, nrow = h, ncol = ncol(net.test))
-
-for(i in 1:nrow(fc.arima)){
-  f.1 <- matrix(as.numeric(fc.arima[i,]), ncol = 1, nrow = ncol(fc.arima))
-  fc.mint.shrink.arima [i,] <- SP %*% f.1
-}
-colnames(fc.mint.shrink.arima) <- colnames(net.test)
+fc.CG.null <- CG(fc.arima, smatrix.net, weights = NULL)
+write.csv(t(fc.CG.null), 'fc.rec.CG.null.arima.csv')
+fc.CG <- CG(fc.arima, smatrix.net, weights = weights)
+write.csv(t(fc.CG), 'fc.rec.CG.shrink.arima.csv')
 
 
-# bind the datasARIMA into a single data frame
-merged_arima <- rbind(cbind(reshape2::melt(net.test), data = 'test.set'), cbind(reshape2::melt(fc.arima), data = 'arima'),
-                    cbind(reshape2::melt(fc.arima.rec), data = 'arima.rec'), 
-                    cbind(reshape2::melt(fc.mint.shrink.arima), data = 'mint.shrink.arima'))
 
-merged_arima%>%
-  filter(Var2 == "Mleccha.in") %>%
-  ggplot(aes(x = Var1, y = value, colour = data)) +
-  geom_line() +
-  xlab("Horizon") +
-  ylab("Count") +
-  theme_bw()
-
-
-  
-## base forecasts using ETS
-
-## training and test sets
-# Splitting data into training and test sets
-net.train <- window(ngts.net, end = c(2021, 12))
-net.test <- window(ngts.net, start = c(2022, 1))
-h <- 12 ## number of forecast points
-fc.ETS <- matrix(NA, nrow = nrow(net.test), ncol = ncol(net.test))
-train.error <- matrix(NA, nrow = nrow(net.train), ncol = ncol(net.train))
+## OLS
+fc.ols <- matrix(NA, nrow = nrow(net.test), ncol = ncol(net.test))
+train.fit <- matrix(NA, nrow = nrow(net.train)-1, ncol = ncol(net.train))
 for(i in seq(NCOL(net.train))){
-  fc <- forecast(ets(net.train[,i]), h = h)
-  fc.ETS[,i] <- fc$mean
-  train.error[,i] <- fc$fitted - net.train[,i]
+  fc.1 <- olsfc.pwfc(net.train[,i], h, breakpoints = c(5, 10, 15, 20), maxlag = 12, nolag = c(1))
+  fc.ols[,i] <- fc.1[[1]]
+  train.fit[,i] <- fc.1[[2]]
 }
-colnames(fc.ETS) <- colnames(net.test)
-colnames(train.error) <- colnames(net.train)
+colnames(fc.ols) <- colnames(net.test)
+colnames(train.fit) <- colnames(net.train)
 
-## computing reconciliation matrix - simplest type
-lambda <- diag(rowSums(smatrix.net))
+write.csv(fc.ols, 'fc.ols.unrec.csv')
+## residuals
+res <- as.matrix(as.data.frame(net.train)[-1,] - train.fit)
+write.csv(res, 'res.train.ols.csv')
+tar <- lowerD(res)
+shrink <- shrink.estim(res, tar)
+w.1 <- shrink[[1]]
+lambda <- shrink[[2]]
+weights <-  methods::as(w.1, "sparseMatrix")
 
-rec.adj.lambda <- as.matrix(smatrix.net%*%solve(t(smatrix.net)%*%solve(lambda)%*%smatrix.net)%*%t(smatrix.net)%*%solve(lambda))
+fc.CG.null <- CG(fc.ols, smatrix.net, weights = NULL)
+write.csv(t(fc.CG.null), 'fc.rec.CG.null.ols.csv')
+fc.CG <- CG(fc.ols, smatrix.net, weights = weights)
+write.csv(t(fc.CG), 'fc.rec.CG.shrink.ols.csv')
 
-fc.ETS.rec <- matrix(NA, nrow = h, ncol = ncol(net.test))
-for(i in 1:nrow(fc.ETS)){
-  f.1 <- matrix(as.numeric(fc.ETS[i,]), ncol = 1, nrow = ncol(fc.ETS))
-  fc.ETS.rec [i,] <- rec.adj.lambda %*% f.1
+
+## NAIVE
+fc.naive <- matrix(NA, nrow = nrow(net.test), ncol = ncol(net.test))
+train.fit <- matrix(NA, nrow = nrow(net.train), ncol = ncol(net.train))
+for(i in seq(NCOL(net.train))){
+  fc.1 <- naive(net.train[,i], h)
+  fc.naive[,i] <- fc.1$mean
+  train.fit[,i] <- fc.1$fitted
 }
-colnames(fc.ETS.rec ) <- colnames(net.test)
+colnames(fc.naive) <- colnames(net.test)
+colnames(train.fit) <- colnames(net.train)
 
-## computing reconciliation matrix - mint_shrink 
-n <- nrow(train.error)
-covm <- crossprod(stats::na.omit(train.error)) / n
-tar <- diag(apply(train.error, 2, compose(crossprod, stats::na.omit))/n)
-corm <- cov2cor(covm)
-xs <- scale(train.error, center = FALSE, scale = sqrt(diag(covm)))
-xs <- xs[stats::complete.cases(xs),]
-v <- (1/(n * (n - 1))) * (crossprod(xs^2) - 1/n * (crossprod(xs))^2)
-diag(v) <- 0
-corapn <- cov2cor(tar)
-d <- (corm - corapn)^2
-lambda <- sum(v)/sum(d)
-lambda <- max(min(lambda, 1), 0)
-W <- lambda * tar + (1 - lambda) * covm
-R <- t(smatrix.net)%*%solve(W)
-P <- Matrix::solve(R%*%smatrix.net)%*%R
-SP <- as.matrix(smatrix.net%*%P)
+write.csv(fc.naive, 'fc.naive.unrec.csv')
+## residuals
+res <- as.matrix(as.data.frame(net.train)[-1,] - (train.fit)[-1,])
+write.csv(res, 'res.train.naive.csv')
+tar <- lowerD(res)
+shrink <- shrink.estim(res, tar)
+w.1 <- shrink[[1]]
+lambda <- shrink[[2]]
+weights <-  methods::as(w.1, "sparseMatrix")
 
-## multiply the based forecasts by SP matrix
+fc.CG.null <- CG(fc.naive, smatrix.net, weights = NULL)
+write.csv(t(fc.CG.null), 'fc.rec.CG.null.naive.csv')
+fc.CG <- CG(fc.naive, smatrix.net, weights = weights)
+write.csv(t(fc.CG), 'fc.rec.CG.shrink.naive.csv')
 
-fc.mint.shrink.ETS <- matrix(NA, nrow = h, ncol = ncol(net.test))
 
-for(i in 1:nrow(fc.ETS)){
-  f.1 <- matrix(as.numeric(fc.ETS[i,]), ncol = 1, nrow = ncol(fc.ETS))
-  fc.mint.shrink.ETS [i,] <- SP %*% f.1
+## ETS
+fc.ets <- matrix(NA, nrow = nrow(net.test), ncol = ncol(net.test))
+train.fit <- matrix(NA, nrow = nrow(net.train), ncol = ncol(net.train))
+for(i in seq(NCOL(net.train))){
+  fc.1 <- forecast(ets(net.train[,i]), h)
+  fc.ets[,i] <- fc.1$mean
+  train.fit[,i] <- fc.1$fitted
 }
-colnames(fc.mint.shrink.ETS) <- colnames(net.test)
+colnames(fc.ets) <- colnames(net.test)
+colnames(train.fit) <- colnames(net.train)
+
+write.csv(fc.ets, 'fc.ets.unrec.csv')
+## residuals
+res <- as.matrix(as.data.frame(net.train) - (train.fit)[-1,])
+write.csv(res, 'res.train.ets.csv')
+tar <- lowerD(res)
+shrink <- shrink.estim(res, tar)
+w.1 <- shrink[[1]]
+lambda <- shrink[[2]]
+weights <-  methods::as(w.1, "sparseMatrix")
+
+fc.CG.null <- CG(fc.ets, smatrix.net, weights = NULL)
+write.csv(t(fc.CG.null), 'fc.rec.CG.null.ets.csv')
+fc.CG <- CG(fc.ets, smatrix.net, weights = weights)
+write.csv(t(fc.CG), 'fc.rec.CG.shrink.ets.csv')
 
 
-# bind the datasets into a single data frame
-merged_ETS <- rbind(cbind(reshape2::melt(net.test), data = 'test.set'), cbind(reshape2::melt(fc.ETS), data = 'ETS'),
-                    cbind(reshape2::melt(fc.ETS.rec), data = 'ETS.rec'), 
-                    cbind(reshape2::melt(fc.mint.shrink.ETS), data = 'mint.shrink.ETS'))
+## Plot the results
+#merged_arima <- rbind(cbind(reshape2::melt(net.test), data = 'test.set'), cbind(reshape2::melt(fc.arima), data = 'arima'), 
+#                     cbind(reshape2::melt(t(fc.CG)), data = 'arima.rec'))
+#merged_arima_train <- rbind(cbind(reshape2::melt(net.train[-1,]), data = 'train.set'), cbind(reshape2::melt(train.fit), data = 'arima'))
 
-merged_ETS%>%
- filter(Var2 == "Total.in") %>%
-  ggplot(aes(x = Var1, y = value, colour = data)) +
-  geom_line() +
-  xlab("Horizon") +
-  ylab("Count") +
-  theme_bw()
+#merged_arima_train%>%
+# filter(Var2 == "Total.in") %>%
+# ggplot(aes(x = Var1, y = value, colour = data)) +
+# geom_line() +
+# xlab("Horizon") +
+# ylab("Count") +
+# theme_bw()
+
+#merged_arima%>%
+#  filter(Var2 == "Total.in") %>%
+#  ggplot(aes(x = Var1, y = value, colour = data)) +
+#  geom_line() +
+#  xlab("Horizon") +
+#  ylab("Count") +
+#  theme_bw()
 
 
+# Extra codes
+# bind the datasARIMA into a single data frame
+#tst1 <- cbind(reshape2::melt(net.test), data = 'actual', count = rep(1:12, 1140))[,-1]
+#colnames(inverted_data.test) <- colnames(net.test)
+#tst2 <- cbind(reshape2::melt(inverted_data.test), data = 'arima', count = rep(1:12, 1140))
+#rec.fore.CHOL <- t(rec.fore.CHOL)
+#fc.CG <- t(fc.CG)
+#colnames(fc.CG) <- colnames(net.test)
+#colnames(rec.fore.CHOL) <- colnames(net.test)
+#tst3 <- cbind(reshape2::melt(rec.fore.CHOL), data = 'arima.rec', count = rep(1:12, 1140))[,-1]
+#tst4 <- cbind(reshape2::melt(fc.CG), data = 'arima.rec.CG', count = rep(1:12, 1140))[,-1]
 
+#colnames(tst3) <- c("series", "value", "data", "count")
+#merged_arima <- rbind(tst1, tst2, tst3, tst4)
 
-
-
+#merged_arima%>%
+#  filter(series == "Total.out") %>%
+# ggplot(aes(x = count, y = value, colour = data)) +
+#geom_line() +
+#xlab("Horizon") +
+#ylab("Count") +
+#theme_bw()
 
 
 
