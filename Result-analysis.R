8e7be5e14b7712ebf16d42e9abb6befeb09b31b9
Mahsa Ashouri <mahsa.ashouri@iss.nthu.edu.tw>
2023-10-12 19:59:24 -0400

    Minor changes to run the results

--- Result-analysis.R
+++ Result-analysis.R
@@ -181,7 +181,7 @@ fc.all <- bind_rows(fc.all %>%
                     fc.all %>% filter(!grepl('\\.in$|\\.out$|Outer$', Series)) %>%
                       mutate (Level = 'Bottom level'))
 
-fc.all <- bind_cols(fc.all, 'id' = rep(1:12, nrow(fc.all)/12))
+fc.all <- bind_cols(fc.all, 'id' = rep(1:4, nrow(fc.all)/4))
 
 # Group by Level, rec_unrec, and method, then calculate RMSE
 rmse_results <- fc.all %>%
