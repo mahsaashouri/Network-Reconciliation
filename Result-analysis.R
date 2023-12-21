f4fab06aed614d9d9f627602cc964dc82d54caa5
Mahsa Ashouri <mahsa.ashouri@iss.nthu.edu.tw>
2023-12-21 10:26:32 -0500

    Added BU rec

--- Result-analysis.R
+++ Result-analysis.R
@@ -32,6 +32,18 @@ fc.train.ets.unrec <- res.train.ets[-1,] + res.train.ets[-1,]
 fc.train.ols.unrec <- res.train.ols + res.train.ols
 fc.train.stl.unrec <- res.train.stl[-1,] + res.train.stl[-1,]
 
+## rec fc - BU
+BU.arima <- read_csv('fc.rec.BU.arima.csv')[,-1]
+BU.ets <- read_csv('fc.rec.BU.ets.csv')[,-1]
+BU.ols <- read_csv('fc.rec.BU.ols.csv')[,-1]
+BU.stl <- read_csv('fc.rec.BU.stl.csv')[,-1]
+
+## rec fc - BU - res
+res.BU.arima <- net.test - BU.arima
+res.BU.ets <- net.test - BU.ets
+res.BU.ols <- net.test - BU.ols
+res.BU.stl <- net.test - BU.stl
+
 ## rec fc - lambda weight
 CG.lambda.arima <- read_csv('fc.rec.CG.lambda.arima.csv')[,-1]
 CG.lambda.ets <- read_csv('fc.rec.CG.lambda.ets.csv')[,-1]
@@ -43,6 +55,18 @@ res.CG.lambda.arima <- net.test - CG.lambda.arima
 res.CG.lambda.ets <- net.test - CG.lambda.ets
 res.CG.lambda.ols <- net.test - CG.lambda.ols
 res.CG.lambda.stl <- net.test - CG.lambda.stl
+
+## rec fc - var weight
+CG.var.arima <- read_csv('fc.rec.CG.var.arima.csv')[,-1]
+CG.var.ets <- read_csv('fc.rec.CG.var.ets.csv')[,-1]
+CG.var.ols <- read_csv('fc.rec.CG.var.ols.csv')[,-1]
+CG.var.stl <- read_csv('fc.rec.CG.var.stl.csv')[,-1]
+
+## rec fc - var weight - res
+res.CG.var.arima <- net.test - CG.var.arima
+res.CG.var.ets <- net.test - CG.var.ets
+res.CG.var.ols <- net.test - CG.var.ols
+res.CG.var.stl <- net.test - CG.var.stl
 
 ## rec fc - shrink weight
 rec.CG.shrink.arima <- read_csv('fc.rec.CG.shrink.arima.csv')[,-1]
@@ -73,10 +97,20 @@ ets.unrec.melt <- reshape2::melt(ets.unrec)
 ols.unrec.melt <- reshape2::melt(ols.unrec)
 stl.unrec.melt <- reshape2::melt(stl.unrec)
 
+BU.arima.melt <- reshape2::melt(BU.arima)
+BU.ets.melt <- reshape2::melt(BU.ets)
+BU.ols.melt <- reshape2::melt(BU.ols)
+BU.stl.melt <- reshape2::melt(BU.stl)
+
 CG.lambda.arima.melt <- reshape2::melt(CG.lambda.arima)
 CG.lambda.ets.melt <- reshape2::melt(CG.lambda.ets)
 CG.lambda.ols.melt <- reshape2::melt(CG.lambda.ols)
 CG.lambda.stl.melt <- reshape2::melt(CG.lambda.stl)
+
+CG.var.arima.melt <- reshape2::melt(CG.var.arima)
+CG.var.ets.melt <- reshape2::melt(CG.var.ets)
+CG.var.ols.melt <- reshape2::melt(CG.var.ols)
+CG.var.stl.melt <- reshape2::melt(CG.var.stl)
 
 rec.CG.shrink.arima.melt <- reshape2::melt(rec.CG.shrink.arima)
 rec.CG.shrink.ets.melt <- reshape2::melt(rec.CG.shrink.ets)
@@ -93,10 +127,20 @@ res.ets.unrec.melt <- reshape2::melt(res.ets.unrec)
 res.ols.unrec.melt <- reshape2::melt(res.ols.unrec)
 res.stl.unrec.melt <- reshape2::melt(res.stl.unrec)
 
+res.BU.arima.melt <- reshape2::melt(res.BU.arima)
+res.BU.ets.melt <- reshape2::melt(res.BU.ets)
+res.BU.ols.melt <- reshape2::melt(res.BU.ols)
+res.BU.stl.melt <- reshape2::melt(res.BU.stl)
+
 res.CG.lambda.arima.melt <- reshape2::melt(res.CG.lambda.arima)
 res.CG.lambda.ets.melt <- reshape2::melt(res.CG.lambda.ets)
 res.CG.lambda.ols.melt <- reshape2::melt(res.CG.lambda.ols)
 res.CG.lambda.stl.melt <- reshape2::melt(res.CG.lambda.stl)
+
+res.CG.var.arima.melt <- reshape2::melt(res.CG.var.arima)
+res.CG.var.ets.melt <- reshape2::melt(res.CG.var.ets)
+res.CG.var.ols.melt <- reshape2::melt(res.CG.var.ols)
+res.CG.var.stl.melt <- reshape2::melt(res.CG.var.stl)
 
 res.rec.CG.shrink.arima.melt <- reshape2::melt(res.rec.CG.shrink.arima)
 res.rec.CG.shrink.ets.melt <- reshape2::melt(res.rec.CG.shrink.ets)
@@ -122,6 +166,18 @@ fc.all <- bind_rows(bind_cols('fc' = arima.unrec.melt$value, 'error' = res.arima
                 bind_cols('fc' = stl.unrec.melt$value, 'error' = res.stl.unrec.melt$value,  
                 'Method' = rep('stl', nrow(stl.unrec.melt)),
                 'Series' =arima.unrec.melt$variable, 'Rec' = 'unrec'),
+                bind_cols('fc' = BU.arima.melt$value, 'error' = res.BU.arima.melt$value,  
+                'Method' = rep('arima', nrow(BU.arima.melt)),
+                'Series' = arima.unrec.melt$variable, 'Rec' = 'rec.BU'), 
+                bind_cols('fc' = BU.ets.melt$value, 'error' = res.BU.ets.melt$value,  
+                'Method' = rep('ets', nrow(BU.ets.melt)),
+                'Series' = arima.unrec.melt$variable, 'Rec' = 'rec.BU'), 
+                bind_cols('fc' = BU.ols.melt$value, 'error' = res.BU.ols.melt$value,  
+                'Method' = rep('ols', nrow(BU.ols.melt)),
+                'Series' = arima.unrec.melt$variable, 'Rec' = 'rec.BU'), 
+                bind_cols('fc' = BU.stl.melt$value, 'error' = res.BU.stl.melt$value,  
+                'Method' = rep('stl', nrow(BU.stl.melt)),
+                'Series' = arima.unrec.melt$BUiable, 'Rec' = 'rec.BU'),
                 bind_cols('fc' = CG.lambda.arima.melt$value, 'error' = res.CG.lambda.arima.melt$value,  
                 'Method' = rep('arima', nrow(CG.lambda.arima.melt)),
                 'Series' = arima.unrec.melt$variable, 'Rec' = 'rec.lambda'), 
@@ -133,7 +189,19 @@ fc.all <- bind_rows(bind_cols('fc' = arima.unrec.melt$value, 'error' = res.arima
                 'Series' = arima.unrec.melt$variable, 'Rec' = 'rec.lambda'), 
                 bind_cols('fc' = CG.lambda.stl.melt$value, 'error' = res.CG.lambda.stl.melt$value,  
                 'Method' = rep('stl', nrow(CG.lambda.stl.melt)),
-                'Series' = arima.unrec.melt$variable, 'Rec' = 'rec.lambda'),
+                'Series' = arima.unrec.melt$lambdaiable, 'Rec' = 'rec.lambda'),
+                bind_cols('fc' = CG.var.arima.melt$value, 'error' = res.CG.var.arima.melt$value,  
+                'Method' = rep('arima', nrow(CG.var.arima.melt)),
+                'Series' = arima.unrec.melt$variable, 'Rec' = 'rec.var'), 
+                bind_cols('fc' = CG.var.ets.melt$value, 'error' = res.CG.var.ets.melt$value,  
+                'Method' = rep('ets', nrow(CG.var.ets.melt)),
+                'Series' = arima.unrec.melt$variable, 'Rec' = 'rec.var'), 
+                bind_cols('fc' = CG.var.ols.melt$value, 'error' = res.CG.var.ols.melt$value,  
+                'Method' = rep('ols', nrow(CG.var.ols.melt)),
+                'Series' = arima.unrec.melt$variable, 'Rec' = 'rec.var'), 
+                bind_cols('fc' = CG.var.stl.melt$value, 'error' = res.CG.var.stl.melt$value,  
+                'Method' = rep('stl', nrow(CG.var.stl.melt)),
+                'Series' = arima.unrec.melt$variable, 'Rec' = 'rec.var'),
                 bind_cols('fc' = rec.CG.shrink.arima.melt$value, 'error' = res.rec.CG.shrink.arima.melt$value,  
                 'Method' = rep('arima', nrow(rec.CG.shrink.arima.melt)),
                 'Series' = arima.unrec.melt$variable, 'Rec' = 'rec.shrink'), 
@@ -181,7 +249,7 @@ fc.all <- bind_rows(fc.all %>%
                     fc.all %>% filter(!grepl('\\.in$|\\.out$|Outer$', Series)) %>%
                       mutate (Level = 'Bottom level'))
 
-fc.all <- bind_cols(fc.all, 'id' = rep(1:4, nrow(fc.all)/4))
+fc.all <- bind_cols(fc.all, 'id' = rep(1:6, nrow(fc.all)/6))
 
 # Group by Level, rec_unrec, and method, then calculate RMSE
 rmse_results_all <- fc.all %>%
@@ -235,10 +303,30 @@ boxplot.stat <- function(x) {
   return(stats)
 }
 
-ggplot(filtered_data, aes(x = id, y = error/100000, fill = id)) +
+ggplot(error.all, aes(x = id, y = error/100000, fill = id)) +
   #geom_boxplot() +
   stat_summary(fun.data = boxplot.stat, geom = "boxplot", alpha = 0.5) +
   facet_wrap(~ Level) +
+  labs(y = "Value") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+error.all.total <- fc.all %>%
+  select(error, Method, Rec) %>%
+  #filter(Rec != "rec.lambda" & Method !="actual") %>%
+  filter(Method !="actual") %>%
+  mutate(id = factor(paste(Method, Rec, sep = "."),
+                     levels = c("ets.rec.shrink", "ets.rec.null", "ets.rec.lambda", "ets.unrec", 
+                                "arima.rec.shrink", "arima.rec.null","arima.rec.lambda","arima.unrec", 
+                                "ols.rec.shrink", "ols.rec.null","ols.rec.lambda","ols.unrec", 
+                                "stl.rec.shrink", "stl.rec.null","stl.rec.lambda","stl.unrec"),
+                     labels = c("ets.rec.shrink", "ets.rec.null", "ets.rec.lambda", "ets.unrec", 
+                                "arima.rec.shrink", "arima.rec.null","arima.rec.lambda","arima.unrec", 
+                                "ols.rec.shrink", "ols.rec.null","ols.rec.lambda","ols.unrec", 
+                                "stl.rec.shrink", "stl.rec.null","stl.rec.lambda","stl.unrec")))
+
+ggplot(error.all.total, aes(x = id, y = error/100000, fill = id)) +
+  geom_boxplot() +
+  #stat_summary(fun.data = boxplot.stat, geom = "boxplot", alpha = 0.5) +
   labs(y = "Value") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
@@ -249,7 +337,7 @@ ggplot(filtered_data, aes(x = scale(error), color = id)) +
   xlim(c(-0.02,0.005))
 
 filtered_data <- fc.all %>%
-  filter(Series == 'other::World_Thinking_Day')
+  filter(Series == 'Outer')
 
 ggplot(filtered_data, aes(x = id, y = fc, color = Method, linetype = Rec)) +
   geom_line() +
